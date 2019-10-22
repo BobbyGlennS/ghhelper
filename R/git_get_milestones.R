025c05ca84acb0a395403c60dc7cf10bdbe1cd36
Xavier <xavier.adam@pm.me>
2019-10-22 21:57:51 +0200

    create_milestone now accept real date for due_on

--- R/git_get_milestones.R
+++ R/git_get_milestones.R
@@ -28,7 +28,12 @@ get_milestones <- function(owner, repo, res_type = "tibble") {
 create_milestone <- function(owner, repo, milestone_content) {
   stopifnot(!is.null(milestone_content$title),
             !is.null(milestone_content$due_on),
+            lubridate::is.Date(milestone_content$due_on),
             is.null(milestone_content$url))
+
+  milestone_content$due_on <- format(milestone_content$due_on,
+                                     "%Y-%m-%dT%H:%M:%S")
+
   url <- stringr::str_glue("/repos/{owner}/{repo}/milestones")
   do.call(POST_gh, append(list(url=url), milestone_content))
 }
